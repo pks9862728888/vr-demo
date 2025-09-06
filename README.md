686f1cdaeab20f4ca271117815d831a8761d4138
pks9862728888 <pks9862728888@gmail.com>
2025-09-06 21:03:54 +0530

    .

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 .eslintrc and .prettierrc can be added for linting and auto-formatting.
 
+## Hooks from react
+
+- useEffect
+- useRef
+
+## Hooks from react-three-fiber
+
+- useFrame
+
+```
+useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x += 1 * delta
+      ref.current.rotation.y += 0.5 * delta
+    }
+  })
+```
+
+### Mesh
+
+Contains geometry and a material
+
 ## References
 
 - [react-three-fiber] (https://www.udemy.com/course/react-three-fiber/learn/lecture/34486996#overview)
--- react-three-fiber-demo1/src/app/_components/Box.tsx
+++ react-three-fiber-demo1/src/app/_components/Box.tsx
@@ -1,6 +1,17 @@
-const Box = () => {
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+
+const Box = (props: any) => {
+  const ref = useRef<any>(undefined)
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x += 1 * delta
+      ref.current.rotation.y += 0.5 * delta
+      // ref.current.position.y = Math.sin(_.clock.getElapsedTime() * 2) / 4
+    }
+  })
   return (
-    <mesh>
+    <mesh {...props} ref={ref}>
       <boxGeometry />
       <meshBasicMaterial color={0x00ff00} wireframe />
     </mesh>
