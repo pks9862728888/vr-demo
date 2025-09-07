3702bde25f8c059925e8f590c5ab31401f953333
pks9862728888 <pks9862728888@gmail.com>
2025-09-07 14:27:08 +0530

    .

--- react-three-fiber-demo1/src/app/_components/CustomCanvas.tsx
+++ react-three-fiber-demo1/src/app/_components/CustomCanvas.tsx
@@ -1,11 +1,23 @@
 import { Canvas } from '@react-three/fiber'
+import { useMemo } from 'react'
+import * as THREE from 'three'
 import Polyhedron from './Polyhedron'
 
 const CustomCanvas = () => {
+  const geometry = useMemo(
+    () => [
+      new THREE.BoxGeometry(),
+      new THREE.SphereGeometry(0.785398),
+      new THREE.DodecahedronGeometry(0.785398)
+    ],
+    []
+  )
   return (
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <Polyhedron position={[-0.75, 0, 0]} name="A" />
-      <Polyhedron position={[0.75, 0, 0]} name="B" />
+    <Canvas camera={{ position: [0, 0, 3] }}>
+      <Polyhedron position={[-0.75, -0.75, 0]} name="A" geometry={geometry} />
+      <Polyhedron position={[0.75, -0.75, 0]} name="B" geometry={geometry} />
+      <Polyhedron position={[-0.75, 0.75, 0]} name="C" geometry={geometry} />
+      <Polyhedron position={[0.75, 0.75, 0]} name="D" geometry={geometry} />
     </Canvas>
   )
 }
