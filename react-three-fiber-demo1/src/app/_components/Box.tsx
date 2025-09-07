f42fd98265519570fb911ed6fd583b4b9c690f1f
pks9862728888 <pks9862728888@gmail.com>
2025-09-07 14:04:26 +0530

    .

--- react-three-fiber-demo1/src/app/_components/Box.tsx
+++ react-three-fiber-demo1/src/app/_components/Box.tsx
@@ -1,14 +1,22 @@
 import { useFrame } from '@react-three/fiber'
-import { useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import * as THREE from 'three'
 
 const Box = (props: any) => {
   const ref = useRef<any>(undefined)
   const [hovered, setHovered] = useState(false)
   const [rotate, setRotate] = useState(false)
+  const geometry = useMemo(() => new THREE.BoxGeometry(), [])
+  useEffect(() => {
+    if (ref.current) {
+      console.log(ref.current.geometry.uuid)
+    }
+  })
   useFrame((_, delta) => {
-    if (ref.current && rotate) {
-      ref.current.rotation.x += 1 * delta
-      ref.current.rotation.y += 0.5 * delta
+    if (ref.current) {
+      ref.current.rotation.x += delta * (rotate ? 1 : 0)
+      ref.current.rotation.y += 0.5 * delta * (rotate ? 1 : 0)
+      // ref.current.rotation.z += delta * (rotate ? 1 : 0)
       // ref.current.position.y = Math.sin(_.clock.getElapsedTime() * 2) / 4
     }
   })
@@ -18,10 +26,11 @@ const Box = (props: any) => {
       ref={ref}
       scale={hovered ? [1.1, 1.1, 1.1] : [1, 1, 1]}
       onPointerDown={(e: any) => setRotate(!rotate)}
-      onUpdate={(self: any) => console.log('props onUpdate', self)}
+      // onUpdate={(self: any) => console.log('props onUpdate', self)}
       onPointerOver={(e: any) => setHovered(true)}
-      onPointerOut={(e: any) => setHovered(false)}>
-      <boxGeometry />
+      onPointerOut={(e: any) => setHovered(false)}
+      geometry={geometry}>
+      {/* <boxGeometry /> */}
       <meshBasicMaterial
         color={hovered ? 0xff0000 : 0x00ff00}
         wireframe
